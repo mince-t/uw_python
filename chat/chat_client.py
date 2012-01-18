25d56cc9f448ff7f93d17bcbf2aa4471ba3705a1
Tony Mince <tonym@mostgifted.com>
2012-01-17 17:21:27 -0800

    update chat directory and code

--- chat/chat_client.py
+++ chat/chat_client.py
@@ -1,10 +1,18 @@
 """
-echo client, usage:
+Assigment 2
 
- python echo_client.py <host> <port>
+In a new chat subdirectory, write a chat server based on
+echo_server_select.  When any client sends a message, that message is
+echoed to all clients, prefixed by the identification of the client
+that sent the message (as well as the server that sent the message).
 
-Both host and port are optional, defaults: localhost 50000
-host must be present if you want to provide port
+Test your chat server with two or more recho clients on localhost.
+Will these servers work as intended with the recho client?  Why not?
+If not, write a chat client, test your chat server with two or more
+chat clients on local host, then run them on your VM.
+
+Jon: This my client.  It only works with one client per ip.
+Should I work on extending this to ip/port?
 """
 
 import select
@@ -12,6 +20,8 @@ import socket
 import sys
 import time
 import datetime
+
+#Send data to a given host on a given port
 def send_data(host,port,data_to_send):
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
     s.connect((host,port)) 
@@ -23,15 +33,23 @@ def send_data(host,port,data_to_send):
     return data
 
 
-host = '192.168.100.79'#'block335046-zme.blueboxgrid.com' 
+host = 'localhost'#'block335046-zme.blueboxgrid.com'
+
+#get the server name from the user
+new_host=raw_input("Enter chat server")
+if new_host !='' :
+    host=new_host
 port = 50003
 listen_port=50004
 size = 1024 
 backlog = 5
+
+#Set up a listener for messages from other users
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
 server.bind(('',listen_port))
+
+
 print
 print
 print '   chat CLIENT'
@@ -46,63 +64,46 @@ server.listen(backlog)
 timeout = 10 # seconds
 input = [server,sys.stdin]
 
-
-
-nargs = len(sys.argv)
-if nargs > 1:
-    host = sys.argv[1]
-if nargs > 2:
-    port = int(sys.argv[2])
-
 input_string=''
+
+#Prompt the user to start chatting
 print 'Enter the text to send(just hit enter to stop):'
 while input_string!='\n':
+
+
     response=''
+    #if we have input from the keyboard
     if input_string!='':
-        
-        response=send_data(host,port,input_string)
+       #Send the input to the server
+       response=send_data(host,port,input_string.strip('\n')
     
     input_string=''
     
-        
-    
-
-  
-
-    
+    #Wait for input/messages from the server
     inputready,outputready,exceptready = select.select(input,[],[],timeout)
-    
-        # timeout
-        
 
-
+    #take a look at our input,if any
     for s in inputready:
         
         if s == server:
             # handle the server socket
             
             client, address = server.accept()
-            
-                        
             input.append(client)
 
-           
-            
-
         elif s == sys.stdin:
-            # handle standard input
-            junk = sys.stdin.readline()
+
+
+            #If the user entered something, grab it to send to the server
+            #If they just hit enter, we'll pop out of the loop
+            input_string=sys.stdin.readline()
             
-            input_string=junk
-            
-           
-
-        elif s: # client socket
-
-
+        elif s: #The server sent us a message
             data = s.recv(size)
-           
+
+            
             if data:
+                #Echo the message back to the user
                 print
                 print  data.strip('\n')
                 
